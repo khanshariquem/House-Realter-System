f78bdb0c856ba421059133813f19ab67fc5e7c3a
Sharique <mkhan8@ncsu.edu>
2018-10-01 22:21:54 -0400

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :companies
   resources :searches, only: [:new, :create, :show]
   resources :users
-  devise_for :users,:path => 'u', :controllers => {:registrations => "registrations"}
+  devise_for :users,:path => 'user', :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#new'
   match '/users/new', to: 'users#new', via: 'get'
