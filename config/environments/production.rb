dca5cd9f75d3be2a60d7938c26d6c13899778dd0
Sharique <mkhan8@ncsu.edu>
2018-10-05 18:43:40 -0400

    Herku destroy try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,12 +20,12 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Program2_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "RealEstateApp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
