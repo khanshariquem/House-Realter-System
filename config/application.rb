c3b0a54bbb75d5204af243e191c39c36ddb8effc
Sharique <mkhan8@ncsu.edu>
2018-10-05 19:15:38 -0400

    Herku destroy try

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Program2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.version = '1.1'
+    # config.assets.version = '1.1'
+    config.assets.initialize_on_precompile = false
   end
 end
