8fb233af2113a0050db0582f8795a73ddef18b79
smadhur <smadhur@ncsu.edu>
2018-10-09 14:06:08 -0400

    Validations added

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,4 +5,15 @@ class House < ApplicationRecord
   has_many :buyers
   validates_presence_of :company_id, :location,:price
   mount_uploader :image, ImageUploader
+  validates :contact, format: { with: /\A([2-9]\d{2})([2-9]\d{2})(\d{4})\z/,
+                              message: 'number invalid'}
+  validates :year,
+            presence: true,
+            inclusion: { in: 1900..Date.today.year },
+            format: {
+                with: /(19|20)\d{2}/i,
+                message: "should be a four-digit year"
+            }
 end
+
+
